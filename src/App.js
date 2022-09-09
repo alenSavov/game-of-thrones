e018b346d62a89346cd487683c19b926f757b16f
Alen Savov <alensavov@gmail.com>
2022-09-09 18:03:20 +0300

    bootstraping Game of Thrones react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Credo Web</h1>
+    <h2>Game of Thrones React js</h2>
+    </div>
+  );
+}
+
+export default App;
