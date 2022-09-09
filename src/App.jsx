896d8fb371044eba88d8d303d5423d81aec0bf47
Alen Savov <alensavov@gmail.com>
2022-09-09 20:18:09 +0300

    added eslint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Credo Web</h1>
+      <h2>Game of Thrones React js</h2>
+    </div>
+  );
+}
+
+export default App;
